28154ff715214d8f490d63b883553219b211c1ac
Nikita <snitkonikita@gmail.com>
2024-08-13 09:40:08 +0200

    feat: add copy and create tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,44 @@
+import fs from 'fs';
+// import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+	const main_dir = './src/fs/files';
+	const copy_dir = './src/fs/files_copy';
+	fs.access(main_dir, err => {
+		if (err) {
+			console.error('FS operation failed');
+		} else {
+			fs.mkdir(copy_dir, err => {
+				if (err) {
+					console.error('FS operation failed');
+				} else {
+					fs.readdir(main_dir, (err, files) => {
+						if (err) console.error(err);
+						if (files) {
+							files.forEach(file => {
+								// const sourcePath = path.join(main_dir, file);
+								// const targetPath = path.join(copy_dir, file);
+
+								const sourcePath_test = `${main_dir}/${file}`;
+								const targetPath_test = `${copy_dir}/${file}`;
+								fs.copyFile(sourcePath_test, targetPath_test, err => {
+									if (err) {
+										console.error(err.message);
+									} else {
+										console.log(`${file} скопирован в ${copy_dir}`);
+										// console.log(sourcePath);
+										// console.log(targetPath);
+										console.log(sourcePath_test);
+										console.log(targetPath_test);
+									}
+								});
+							});
+						}
+					});
+				}
+			});
+		}
+	});
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+	const dir = './src/fs/files/fresh.txt';
+	const text = 'I am fresh and young';
+
+	fs.readFile(dir, err => {
+		if (err) {
+			fs.writeFile(dir, text, () => {});
+			console.info('File add');
+		} else {
+			console.error('FS operation failed');
+		}
+	});
 };
 
 await create();
